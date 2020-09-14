860e737d5941e9f842e9b7ae2a35b2679773aff4
ybw <root@bug-maker.com>
2020-09-14 10:59:55 +0800

    添加云服务

--- Server.php
+++ Server.php
@@ -8,16 +8,12 @@ use esa\Http;
  */
 class Server
 {
-    public static $base_url = "https://www.bug-maker.com/addons/";
-    public static $base_addon = "demo";
+    public static $base_url = "http://api.bug-maker.com/";
     
     // 拼接url
-    public static function url($purl="api/index/index"){
-        $url = explode("/",$purl);
-        if(count($url) != 3){
-            throw Exception('ESA::Server()->url() 参数错误!');
-        }
-        return self::$base_url.self::$base_addon.".".$url[0].".".$url[1]."/".$url[2].".html";
+    public static function url($purl="Sys/get_version"){
+        $url = ucfirst($purl);
+        return self::$base_url.$url.".html";
     }
     
     // 请求处理
@@ -31,5 +27,38 @@ class Server
             "type"  => "get_list",
         ];
         return self::post("api/index/index",$data);
+    }
+    
+    // 本地信息
+    public static function my_info($m = ""){
+        $info_path = APP_PATH."/version";
+        $info = explode(":",@file_get_contents($info_path));
+        if(count($info) != 2){
+            throw new Exception("ESA Admin 版本信息不正确");
+        }
+        $tv = explode("_",$info[1]);
+        if(count($tv) != 2){
+            throw new Exception("ESA Admin 版本信息不正确2");
+        }
+        $res = [
+            "type"      => $tv[1],
+            "version"   => $tv[0],
+        ];
+        return empty($m) ? $res : $res[$m];
+    }
+    
+    // 获取线上版本
+    public static function get_version(){
+        $data = self::my_info();
+        return self::post("sys/get_version",$data);
+    }
+    
+    // 获取线上版本列表
+    public static function get_upgrade($page = 0){
+        $data = [
+            "limit" => 5,
+            "page"  => $page
+        ];
+        return @json_decode(Http::get(self::url("sys/upgrade"),$data),true);
     }
 }
